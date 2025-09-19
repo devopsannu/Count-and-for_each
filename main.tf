dcbf55edfa4bd29eac978a9103de4aaae36ff0cd
terraformcodes <oves.mulla@gmail.com>
2025-09-19 09:40:01 +0530

    count codes

--- main.tf
+++ main.tf
@@ -14,25 +14,25 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "RG1" {
   count    = 3
-  name     = "dev-001"                    
+  name     = "dev-001"                       # 1st seniro Only with count
   location = "east us"
 }
 
 resource "azurerm_resource_group" "RG2" {
   count    = 3
-  name     = count.index                    
+  name     = count.index                   # 2nd seniro with count.index 
   location = "east   us"
 }
 
 resource "azurerm_resource_group" "RG3" {
   count    = 3
-  name     = "oves${count.index}"
+  name     = "oves${count.index}"             # 3rd seniro  with string interpolation       
   location = "east us"
 }
 
 resource "azurerm_resource_group" "RG4" {
   count    = 3
-  name     = var.rgname[count.index]
+  name     = var.rgname[count.index]             # 4th seniro Only with variable
   location = "east us"
 }
 
@@ -42,7 +42,7 @@ variable "rgname" {
 
 resource "azurerm_resource_group" "RG5" {
   count    = length(var.rgname)
-  name     = var.rgname[count.index]
+  name     = var.rgname[count.index]              # 5th seniro  with variable and lenght
   location = "east us"
 }
 
